d0c5b39566abedf823eb2a1049822e882a7c67f2
JFerrer95 <jonathanferrer95@gmail.com>
2020-05-22 20:23:58 -0400

    Initial Commit

---
+++ Fenamenal Game of Life/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fenamenal Game of Life
+//
+//  Created by jonathan ferrer on 5/22/20.
+//  Copyright © 2020 Jonathan Ferrer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
